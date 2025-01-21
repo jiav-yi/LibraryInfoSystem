ff87b3e1e4267ebfed71152fab1bf500ee3c202e
jiav_yi <1831562069@qq.com>
2025-01-21 11:49:15 +0800

    前后端初始化

---
+++ LibraryInfoSystem-back/LibraryInfoSystem-back/src/main/java/com/jnu/LibraryInfoSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.jnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryInfoSystemBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryInfoSystemBackApplication.class, args);
+    }
+
+}
---
+++ LibraryInfoSystem-back/LibraryInfoSystem-back/src/test/java/com/jnu/LibraryInfoSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryInfoSystemBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
